c8301ae2b57a9b44a532dc641e22956447a11255
Avito Developer <avitodeveloper@Avito-MacBook-Pro.local>
2019-02-19 14:01:50 +0100

    work with basic functionality

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -2,19 +2,30 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadsTest extends TestCase
 {
-
     use DatabaseMigrations;
 
-    public function a_user_can_browse_threads()
+    /** @test */
+    public function a_user_can_browse_all_threads()
     {
+        $thread = factory('App\Thread')->create();
+
         $response = $this->get('/threads');
 
-        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_browse_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/'.$thread->id);
+
+        $response->assertSee($thread->title);
     }
 }
