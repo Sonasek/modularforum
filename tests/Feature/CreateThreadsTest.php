ad8142ec85a84f81276cafeeeaf3437a4cb388db
Ondřej Sono Levek <son4s3k@gmail.com>
2019-02-21 15:48:37 +0100

    logged user can add threads

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function a_guests_may_not_create_thread()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $this->withoutExceptionHandling();
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->signIn();
+
+        $thread = make('App\Thread');
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+
+    }
+}
---
+++ tests/Feature/FooTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigration;
+
+class FooTest extends TestCase
+{
+
+    use DatabaseMigration;
+
+    /** @test */
+    public function it_$METHOD()
+    {
+        $END
+    }
+}
